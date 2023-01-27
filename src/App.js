7f9f6f2835db85e249b484aa3773a4f49d696030
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-01-27 23:02:19 +0200

    Weather Website React template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from "./Search";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="main">
+        <div className="main__container">
+          <div className="main__containerTemperature">12°</div>
+          <div className="main__containerInfo">
+            <div className="main__containerInfo-city">London</div>
+            <div className="main__containerInfo-date">
+              06:09 - Saturday, 26 Jan `23
+            </div>
+          </div>
+          <div className="main__containerWeather">
+            <div className="main__containerWeather-icon">
+              <FontAwesomeIcon icon="fa-solid fa-cloud-rain" />{" "}
+            </div>
+            <div className="main__containerWeather-description">Rainy</div>
+          </div>
+        </div>
+      </section>
+      <section className="addition">
+        <div className="addition__search">
+          <Search />
+        </div>
+        <div className="addition__cities">
+          <p>
+            <a href="/">Paris</a>
+          </p>
+          <p>
+            <a href="/">Kyiv</a>
+          </p>
+          <p>
+            <a href="/">Berlin</a>
+          </p>
+        </div>
+        <hr />
+        <div className="addition__description">
+          <div className="addition__description-cloudy">
+            <div>Cloudy</div>
+            <div>86 %</div>
+          </div>
+          <div className="addition__description-humidity">
+            <div>Humidity</div>
+            <div>62 %</div>
+          </div>
+          <div className="addition__description-wind">
+            <div>Wind</div>
+            <div>2 km/h</div>
+          </div>
+          <div className="addition__description-rain">
+            <div>Rain</div>
+            <div>8 mm</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
-export default App;
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+
+export default function Search(){
+    return (
+      <form >
+        <input type="search" placeholder="Another location"/>
+        <input type="submit" value="🔍" />
+      </form>
+    );
+}
