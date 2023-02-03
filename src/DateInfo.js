cd441c9f746b31e43e29df1569866571a17536cc
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-02-03 15:23:28 +0200

    Added a date information

---
+++ src/DateInfo.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { renderIntoDocument } from "react-dom/test-utils";
+
+export default function DateInfo(props) {
+  console.log(props.date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let day = days[props.date.getDay()];
+  let month = months[props.date.getMonth()];
+  let date = props.date.getDate();
+  console.log(date);
+
+  let year = props.date.getFullYear().toString().substring(2);
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes} - ${day}, ${date} ${month} \`${year}`;
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 import "./Weather.css";
 import axios from "axios";
+import DateInfo from "./DateInfo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 
 export default function Weather(props) {
-  
-  let [weather, setWeather] = useState({loaded:false});
+  let [weather, setWeather] = useState({ loaded: false });
 
-
-
-  function handleResponse(response){
+  function handleResponse(response) {
     console.log(response.data);
     setWeather({
-      loaded:true,
+      loaded: true,
       temperature: Math.round(response.data.temperature.current),
       city: response.data.city,
+      date: new Date(response.data.time*1000),
       description: response.data.condition.description,
       feeling: Math.round(response.data.temperature.feels_like),
       humidity: Math.round(response.data.temperature.humidity),
@@ -27,105 +26,98 @@ export default function Weather(props) {
     });
   }
 
-  if(weather.loaded){
-     return (
-       <div className="Weather">
-         <div className="container">
-           <div className="row align-items-center justify-content-center g-0">
-             <div className="main col-sm-6">
-               <div className="main__container">
-                 <div className="main__containerTemperature">
-                   {weather.temperature}°
-                 </div>
-                 <div className="main__containerInfo">
-                   <div className="main__containerInfo-city">
-                     {weather.city}
-                   </div>
+  if (weather.loaded) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="row align-items-center justify-content-center g-0">
+            <div className="main col-sm-6">
+              <div className="main__container">
+                <div className="main__containerTemperature">
+                  {weather.temperature}°
+                </div>
+                <div className="main__containerInfo">
+                  <div className="main__containerInfo-city">{weather.city}</div>
 
-                   <div className="main__containerInfo-date">
-                     06:09 - Saturday, 26 Jan `23
-                   </div>
-                 </div>
+                  <div className="main__containerInfo-date">
+                    <DateInfo date={weather.date}/>
+                    {/* 06:09 - Saturday, 26 Jan `23 */}
+                  </div>
+                </div>
 
-                 <div className="main__containerWeather">
-                   <div className="main__containerWeather-icon">
-                    <img src={weather.icon} alt={weather.description}/>
-                   </div>
+                <div className="main__containerWeather">
+                  <div className="main__containerWeather-icon">
+                    <img src={weather.icon} alt={weather.description} />
+                  </div>
 
-                   <div className="main__containerWeather-description">
-                     {weather.description}
-                   </div>
-                 </div>
-               </div>
-             </div>
-             <div className="addition col-sm-3">
-               <div className="addition_container ps-3">
-                 <div className="addition__search">
-                   <Search />
-                 </div>
+                  <div className="main__containerWeather-description">
+                    {weather.description}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="addition col-sm-3">
+              <div className="addition_container ps-3">
+                <div className="addition__search">
+                  <Search />
+                </div>
 
-                 <div className="addition__cities">
-                   <p>
-                     <a href="/">Paris</a>
-                   </p>
+                <div className="addition__cities">
+                  <p>
+                    <a href="/">Paris</a>
+                  </p>
 
-                   <p>
-                     <a href="/">Kyiv</a>
-                   </p>
+                  <p>
+                    <a href="/">Kyiv</a>
+                  </p>
 
-                   <p>
-                     <a href="/">Berlin</a>
-                   </p>
-                 </div>
-                 <hr />
+                  <p>
+                    <a href="/">Berlin</a>
+                  </p>
+                </div>
+                <hr />
 
-                 <div className="addition__description">
-                   <div className="addition__description-cloudy">
-                     <div>Feels like</div>
-                     <div>{weather.feeling}°</div>
-                   </div>
+                <div className="addition__description">
+                  <div className="addition__description-cloudy">
+                    <div>Feels like</div>
+                    <div>{weather.feeling}°</div>
+                  </div>
 
-                   <div className="addition__description-humidity">
-                     <div>Humidity</div>
-                     <div>{weather.humidity} %</div>
-                   </div>
-                   <div className="addition__description-wind">
-                     <div>Wind</div>
-                     <div>{weather.wind} km/h</div>
-                   </div>
-                   <div className="addition__description-rain">
-                     <div>Rain</div>
-                     <div>8 mm</div>
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
+                  <div className="addition__description-humidity">
+                    <div>Humidity</div>
+                    <div>{weather.humidity} %</div>
+                  </div>
+                  <div className="addition__description-wind">
+                    <div>Wind</div>
+                    <div>{weather.wind} km/h</div>
+                  </div>
+                  <div className="addition__description-rain">
+                    <div>Rain</div>
+                    <div>8 mm</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-         <div className="gitHub">
-           <a
-             href="https://github.com/AnastasiiaPiatak/weather-app-react"
-             target="_blank"
-             rel="noreferrer"
-           >
-             Open-source code
-           </a>{" "}
-           by Anastasiia Piatak
-         </div>
-       </div>
-     );
-  } else{
+        <div className="gitHub">
+          <a
+            href="https://github.com/AnastasiiaPiatak/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Anastasiia Piatak
+        </div>
+      </div>
+    );
+  } else {
     const apiKey = `c6o2a4403fb65ced4d0bd7bea2650b1t`;
-  let city = (props.defaultCity);
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return 'Loading..'
+    let city = props.defaultCity;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..";
   }
-
-
-
-  
-
- 
 }
