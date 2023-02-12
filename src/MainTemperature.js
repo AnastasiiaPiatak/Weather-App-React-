89f96154ac2a0e4f41b88a1f59cf91a73b35c09f
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-02-12 14:02:26 +0200

    Removed celsius to fahrenheit conversion

--- src/MainTemperature.js
+++ src/MainTemperature.js
@@ -1,39 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function MainTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertFahr(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertCels(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="main__containerTemperature MainTemperature">
-        {props.celsius}
-        <a href="/" onClick={convertFahr}>
-          °C
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <div className="main__containerTemperature MainTemperature">
-        {Math.round(fahrenheit())}
-        <a href="/" onClick={convertCels}>
-          °F
-        </a>
-      </div>
-    );
-  }
+  return (
+    <div className="main__containerTemperature MainTemperature">
+      {props.celsius}
+      <span>°</span>
+    </div>
+  );
 }
